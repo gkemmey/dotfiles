b6c1c05c0561e968fba6a5d9d28b769c98513b25
gkemmey <gray.kemmey@gmail.com>
2025-02-04 00:18:42 -0500

    wezterm: autoformat config file

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,22 +21,22 @@ local config = wezterm.config_builder()
 -- base0E: "#a71d5d"
 -- base0F: "#333333"
 local github_colors = {
-  foreground = "#333333", -- 05
-  background = "#ffffff", -- 00
-  cursor_bg = "#333333", -- 05
+  foreground = "#333333",    -- 05
+  background = "#ffffff",    -- 00
+  cursor_bg = "#333333",     -- 05
   cursor_border = "#333333", -- 05
-  cursor_fg = "#333333", -- 05
-  selection_bg = "#333333", -- 05
-  selection_fg = "#ffffff", -- 00
+  cursor_fg = "#333333",     -- 05
+  selection_bg = "#333333",  -- 05
+  selection_fg = "#ffffff",  -- 00
   ansi = {
-    "#ffffff", -- 00
-    "#ed6a43", -- 08
-    "#116329", -- 0B
-    "#9a6700", -- 0A
-    "#795da3", -- 0D
-    "#a71d5d", -- 0E
-    "#183691", -- 0C
-    "#333333"  -- 05
+    "#ffffff",               -- 00
+    "#ed6a43",               -- 08
+    "#116329",               -- 0B
+    "#9a6700",               -- 0A
+    "#795da3",               -- 0D
+    "#a71d5d",               -- 0E
+    "#183691",               -- 0C
+    "#333333"                -- 05
   },
   brights = {
     "#969896", -- 03
@@ -72,12 +72,12 @@ config.colors = {
 
 config.font = wezterm.font_with_fallback({
   { family = "Monego Nerd Font Fix", weight = "Regular", harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' } },
-  { family = "Apple Color Emoji", weight = "Regular" }
+  { family = "Apple Color Emoji",    weight = "Regular" }
 })
 config.cell_width = 0.95
 config.font_size = 14
-config.freetype_load_target = "Light" -- ref: https://github.com/wez/wezterm/issues/3774
-config.freetype_load_flags = "NO_HINTING" -- ref: https://github.com/wez/wezterm/issues/3774
+config.freetype_load_target = "Light"           -- ref: https://github.com/wez/wezterm/issues/3774
+config.freetype_load_flags = "NO_HINTING"       -- ref: https://github.com/wez/wezterm/issues/3774
 config.freetype_render_target = "HorizontalLcd" -- ref: https://github.com/wez/wezterm/issues/3774
 config.line_height = 1.25
 
@@ -133,48 +133,48 @@ config.hyperlink_rules = {
 
 config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-  { key = "d", mods = "LEADER", action = act.ShowDebugOverlay },
-  { key = "s", mods = "LEADER", action = act.ActivateKeyTable { name = "split_mode", one_shot = true } },
+  { key = "d",          mods = "LEADER",      action = act.ShowDebugOverlay },
+  { key = "s",          mods = "LEADER",      action = act.ActivateKeyTable { name = "split_mode", one_shot = true } },
 
-  { key = "P", mods = "SUPER", action = act.ActivateCommandPalette },
-  { key = "P", mods = "SHIFT|SUPER", action = act.ActivateCommandPalette },
-  { key = "p", mods = "SHIFT|SUPER", action = act.ActivateCommandPalette },
+  { key = "P",          mods = "SUPER",       action = act.ActivateCommandPalette },
+  { key = "P",          mods = "SHIFT|SUPER", action = act.ActivateCommandPalette },
+  { key = "p",          mods = "SHIFT|SUPER", action = act.ActivateCommandPalette },
 
-  { key = "k", mods = "SUPER", action = act.ClearScrollback "ScrollbackAndViewport" },
+  { key = "k",          mods = "SUPER",       action = act.ClearScrollback "ScrollbackAndViewport" },
 
-  { key = "c", mods = "SUPER", action = act.CopyTo "Clipboard" },
-  { key = "v", mods = "SUPER", action = act.PasteFrom "Clipboard" },
+  { key = "c",          mods = "SUPER",       action = act.CopyTo "Clipboard" },
+  { key = "v",          mods = "SUPER",       action = act.PasteFrom "Clipboard" },
 
-  { key = "{", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(-1) },
-  { key = "}", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(1) },
-  { key = "[", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(-1) },
-  { key = "]", mods = "SHIFT|SUPER", action = act.ActivateTabRelative(1) },
+  { key = "{",          mods = "SHIFT|SUPER", action = act.ActivateTabRelative(-1) },
+  { key = "}",          mods = "SHIFT|SUPER", action = act.ActivateTabRelative(1) },
+  { key = "[",          mods = "SHIFT|SUPER", action = act.ActivateTabRelative(-1) },
+  { key = "]",          mods = "SHIFT|SUPER", action = act.ActivateTabRelative(1) },
 
-  { key = "{", mods = "SHIFT|OPT", action = act.ActivatePaneDirection("Prev") },
-  { key = "}", mods = "SHIFT|OPT", action = act.ActivatePaneDirection("Next") },
-  { key = "[", mods = "SHIFT|OPT", action = act.ActivatePaneDirection("Prev") },
-  { key = "]", mods = "SHIFT|OPT", action = act.ActivatePaneDirection("Next") },
+  { key = "{",          mods = "SHIFT|OPT",   action = act.ActivatePaneDirection("Prev") },
+  { key = "}",          mods = "SHIFT|OPT",   action = act.ActivatePaneDirection("Next") },
+  { key = "[",          mods = "SHIFT|OPT",   action = act.ActivatePaneDirection("Prev") },
+  { key = "]",          mods = "SHIFT|OPT",   action = act.ActivatePaneDirection("Next") },
 
-  { key = "{", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
-  { key = "}", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
-  { key = "[", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
-  { key = "]", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
+  { key = "{",          mods = "SHIFT|CTRL",  action = act.MoveTabRelative(-1) },
+  { key = "}",          mods = "SHIFT|CTRL",  action = act.MoveTabRelative(1) },
+  { key = "[",          mods = "SHIFT|CTRL",  action = act.MoveTabRelative(-1) },
+  { key = "]",          mods = "SHIFT|CTRL",  action = act.MoveTabRelative(1) },
 
-  { key = 'f', mods = 'SHIFT|CTRL', action = act.Search({ CaseInSensitiveString = '' }) },
-  { key = 'f', mods = 'SUPER', action = act.Search({ CaseInSensitiveString = '' }) },
+  { key = 'f',          mods = 'SHIFT|CTRL',  action = act.Search({ CaseInSensitiveString = '' }) },
+  { key = 'f',          mods = 'SUPER',       action = act.Search({ CaseInSensitiveString = '' }) },
 
-  { key = "LeftArrow", mods = "OPT", action = act.SendKey { key = "b", mods = "ALT" } },
-  { key = "RightArrow", mods = "OPT", action = act.SendKey { key = "f", mods = "ALT" } },
-  { key = "LeftArrow", mods = "SUPER", action = act.SendKey { key = "Home", mods = "NONE" } },
-  { key = "RightArrow", mods = "SUPER", action = act.SendKey { key = "End", mods = "NONE" } },
+  { key = "LeftArrow",  mods = "OPT",         action = act.SendKey { key = "b", mods = "ALT" } },
+  { key = "RightArrow", mods = "OPT",         action = act.SendKey { key = "f", mods = "ALT" } },
+  { key = "LeftArrow",  mods = "SUPER",       action = act.SendKey { key = "Home", mods = "NONE" } },
+  { key = "RightArrow", mods = "SUPER",       action = act.SendKey { key = "End", mods = "NONE" } },
 
-  { key = "UpArrow", mods = "SUPER", action = act.ScrollToTop },
-  { key = "DownArrow", mods = "SUPER", action = act.ScrollToBottom },
+  { key = "UpArrow",    mods = "SUPER",       action = act.ScrollToTop },
+  { key = "DownArrow",  mods = "SUPER",       action = act.ScrollToBottom },
 }
 
 config.key_tables = {
   split_mode = {
-    { key = "-", mods = "", action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
+    { key = "-", mods = "",      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
     { key = "|", mods = "SHIFT", action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
   },
 }
